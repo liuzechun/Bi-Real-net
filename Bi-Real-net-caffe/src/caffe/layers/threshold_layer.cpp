bbaa8e7f856a9bb96bf3f338283e5c7df6da6ec8
liuzechun <362092891@qq.com>
2018-10-21 16:19:37 +0800

    Add files via upload

--- Bi-Real-net-caffe/src/caffe/layers/threshold_layer.cpp
+++ Bi-Real-net-caffe/src/caffe/layers/threshold_layer.cpp
@@ -18,12 +18,30 @@ void ThresholdLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   Dtype* top_data = top[0]->mutable_cpu_data();
   const int count = bottom[0]->count();
   for (int i = 0; i < count; ++i) {
-    top_data[i] = (bottom_data[i] > threshold_) ? Dtype(1) : Dtype(0);
+    top_data[i] = (bottom_data[i] > threshold_) ? Dtype(1) : Dtype(-1);
   }
 }
 
+template <typename Dtype>
+void ThresholdLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  if ( propagate_down[0] == false ) return;
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  const Dtype* top_diff = top[0]->cpu_diff();
+  Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+  for (int index = 0; index < top[0]->count(); index++) {
+    if ( bottom_data[index] >= Dtype(-1) && bottom_data[index] < Dtype(0) ) {
+      bottom_diff[ index ] = (2+2*bottom_data[index])*top_diff[ index ];
+    } else if ( bottom_data[index] >= Dtype(0) && bottom_data[index] < Dtype(1) ) {
+      bottom_diff[ index ] = (2-2*bottom_data[index])*top_diff[ index ];
+    } else {
+      bottom_diff[ index ] = Dtype(0);
+    }
+  } 
+}
+
 #ifdef CPU_ONLY
-STUB_GPU_FORWARD(ThresholdLayer, Forward);
+STUB_GPU(ThresholdLayer);
 #endif
 
 INSTANTIATE_CLASS(ThresholdLayer);
--- Bi-Real-net-caffe/src/caffe/layers/threshold_layer.cu
+++ Bi-Real-net-caffe/src/caffe/layers/threshold_layer.cu
@@ -8,7 +8,7 @@ template <typename Dtype>
 __global__ void ThresholdForward(const int n, const Dtype threshold,
     const Dtype* in, Dtype* out) {
   CUDA_KERNEL_LOOP(index, n) {
-    out[index] = in[index] > threshold ? 1 : 0;
+    out[index] = in[index] > threshold ? 1 : -1;
   }
 }
 
@@ -25,7 +25,33 @@ void ThresholdLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
 }
 
 
-INSTANTIATE_LAYER_GPU_FORWARD(ThresholdLayer);
 
+template <typename Dtype>
+__global__ void ThresholdBackward(const int count, const Dtype* bottom_data, const Dtype* top_diff, Dtype* bottom_diff) {
+  CUDA_KERNEL_LOOP(index, count) {
+    if (bottom_data[index] >= Dtype(-1) && bottom_data[index] < Dtype(0)) {
+      bottom_diff[ index ] = (2+2*bottom_data[index])*top_diff[ index ];
+    } else if (bottom_data[index] >= Dtype(0) && bottom_data[index] < Dtype(1)) {
+      bottom_diff[ index ] = (2-2*bottom_data[index])*top_diff[ index ];
+    } else {
+      bottom_diff[ index ] = Dtype(0);
+    }
+  }
+}
+
+template <typename Dtype>
+void ThresholdLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  if ( propagate_down[0] == false ) return;
+  const Dtype* bottom_data = bottom[0]->gpu_data();
+  const Dtype* top_diff = top[0]->gpu_diff();
+  Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
+  const int count = top[0]->count();
+  ThresholdBackward<Dtype>
+      <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count, bottom_data, top_diff, bottom_diff);
+}
+
+INSTANTIATE_LAYER_GPU_FUNCS(ThresholdLayer);
 
 }  // namespace caffe
